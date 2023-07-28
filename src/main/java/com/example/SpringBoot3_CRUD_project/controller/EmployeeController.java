fe4c74019a1991185b5a4acc1d2c0c0dcee69fb5
Nikita Kezins <n.kezins@student.utwente.nl>
2023-07-28 18:42:02 +0200

    Added Update functionality

--- src/main/java/com/example/SpringBoot3_CRUD_project/controller/EmployeeController.java
+++ src/main/java/com/example/SpringBoot3_CRUD_project/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.example.SpringBoot3_CRUD_project.entity.Employee;
 import com.example.SpringBoot3_CRUD_project.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,6 +36,15 @@ public class EmployeeController {
 	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
 		employeeService.save(employee);
 		return "redirect:/employees/list";
+	}
+
+	@GetMapping("showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int id,
+									Model model) {
+		Employee employee = employeeService.findById(id);
+		model.addAttribute("employee", employee);
+		return "employees/employee-form";
+
 	}
 
 }
