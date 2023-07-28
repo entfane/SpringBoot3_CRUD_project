853c08538b2ed7399d218f35970ec48a7683d5ea
Nikita Kezins <n.kezins@student.utwente.nl>
2023-07-28 14:24:41 +0200

    Modified EmployeeController so that it takes data from the database

--- src/main/java/com/example/SpringBoot3_CRUD_project/controller/EmployeeController.java
+++ src/main/java/com/example/SpringBoot3_CRUD_project/controller/EmployeeController.java
@@ -1,48 +1,29 @@
 package com.example.SpringBoot3_CRUD_project.controller;
 
 import com.example.SpringBoot3_CRUD_project.entity.Employee;
+import com.example.SpringBoot3_CRUD_project.service.EmployeeService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	// load employee data
+	private EmployeeService employeeService;
 
-	private List<Employee> theEmployees;
-
-	@PostConstruct
-	private void loadData() {
-
-		// create employees
-		Employee emp1 = new Employee("Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee("Emma", "Baumgarten", "emma@luv2code.com");
-		Employee emp3 = new Employee("Avani", "Gupta", "avani@luv2code.com");
-
-		// create the list
-		theEmployees = new ArrayList<>();
-
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
 	}
-
-	// add mapping for "/list"
 
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
-
-		// add to the spring model
-		theModel.addAttribute("employees", theEmployees);
-
+		List<Employee> employeeList = employeeService.findAll();
+		theModel.addAttribute("employees", employeeList);
 		return "list-employees";
 	}
 }
