970fa8f845374022eac5b54ab8d643b55535c0fa
Nikita Kezins <n.kezins@student.utwente.nl>
2023-08-12 18:11:12 +0200

    Created LoggingAspect with @Before advice

---
+++ src/main/java/com/example/SpringBoot3_CRUD_project/aspect/LoggingAspect.java
@@ -0,0 +1,35 @@
+package com.example.SpringBoot3_CRUD_project.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Pointcut("execution(* com.example.SpringBoot3_CRUD_project.controller.*.*(..))")
+    private void forControllerPackage() {}
+
+    @Pointcut("execution(* com.example.SpringBoot3_CRUD_project.service.*.*(..))")
+    private void forServicePackage() {}
+
+    @Pointcut("execution(* com.example.SpringBoot3_CRUD_project.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow() {};
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("===>> in @Before: calling method: " + method);
+    }
+
+}
